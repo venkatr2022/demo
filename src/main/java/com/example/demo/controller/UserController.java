5c198d2149459eee375fb4a8b9816e6d4f3e97f9
venkatr2022 <venkat_rao@myyahoo.com>
2025-06-24 17:58:48 -0400

    Updated code.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,28 +4,54 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("/api/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
 
-    @GetMapping("/users")
+    @RequestMapping(value="", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @PostMapping("/addUser")
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void addUser(@RequestBody User user) {
         userRepository.save(user);
 
     }
 
+    @RequestMapping(value = "/addUsers", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void addUser(@RequestBody List<User> users) {
+        userRepository.saveAll(users);
+
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public User getUserById(@PathVariable String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void deleteUser(@PathVariable String id) {
+        userRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public User updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(updatedUser.getName());
+                    user.setEmail(updatedUser.getEmail());
+                    return userRepository.save(user);
+                }).orElse(null);
+    }
 }
